fc030168d85f5faa74e09da13a1afd9e57daf341
Marko Vukosavljević <marko.vukosavljevic@infinum.hr>
2023-01-03 21:41:25 +0100

    Fix issue where extension wouldn't load
    - fix by using `chrome` and not `window.chrome` for access to storage
    - also add changelog to readme
    - also add clean up before build step

--- src/services/injector.ts
+++ src/services/injector.ts
@@ -4,6 +4,6 @@ import { PlayerService } from './player.service';
 import { StorageService } from './storage.service';
 
 export const eventService: EventService = new EventService();
-export const storageService: StorageService = new StorageService(window.chrome.storage.local);
+export const storageService: StorageService = new StorageService(chrome.storage.local);
 export const domService: DomService = new DomService(document, window);
 export const playerService: PlayerService = new PlayerService(domService);
